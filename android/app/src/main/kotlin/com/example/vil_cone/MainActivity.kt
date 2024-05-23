5ea180a9e784e661dda06b97a07f8eb861e24034
Bharat Wakade <155426524+Sisko321@users.noreply.github.com>
2024-05-23 23:39:12 +0530

    StripeSetup&CustomThemeDone

--- android/app/src/main/kotlin/com/example/vil_cone/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vil_cone/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.vil_cone
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
 
 class MainActivity: FlutterActivity() {
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:vil_cone/Components/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -6,6 +10,13 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+//Stripe Setup
+  final String response =
+      await rootBundle.loadString("assets/config/stripe.json");
+  final data = await json.decode(response);
+  Stripe.publishableKey = data["publishablekey"];
+
   runApp(const MyApp());
 }
 
@@ -13,6 +24,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
